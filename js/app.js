ae3ef29634c341365395e9d413510d4d8c677756
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-12-22 00:26:08 +0000

    Add mobile menu gradient overlay

--- js/app.js
+++ js/app.js
@@ -24,11 +24,14 @@ __webpack_require__.r(__webpack_exports__);
 var btn = document.querySelector('[data-toggle]');
 var targetId = btn.getAttribute('aria-controls');
 var target = document.querySelector("#".concat(targetId));
+var menuOverlay = document.querySelector('.menu-overlay');
 btn.addEventListener('click', toggleCollapse);
+menuOverlay.addEventListener('click', toggleCollapse);
 
 function toggleCollapse(e) {
   this.getAttribute('aria-expanded') === 'true' ? this.setAttribute('aria-expanded', 'false') : this.setAttribute('aria-expanded', 'true');
   this.classList.toggle('active');
+  document.body.classList.toggle('menu');
   target.classList.toggle('show');
 }
 
--- src/js/components/collapse.js
+++ src/js/components/collapse.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector('[data-toggle]');
 const targetId = btn.getAttribute('aria-controls');
 const target = document.querySelector(`#${targetId}`);
+const menuOverlay = document.querySelector('.menu-overlay');
 
 btn.addEventListener('click', toggleCollapse);
+menuOverlay.addEventListener('click', toggleCollapse);
 
 function toggleCollapse(e) {
     (this.getAttribute('aria-expanded') === 'true')
@@ -10,6 +12,7 @@ function toggleCollapse(e) {
         : this.setAttribute('aria-expanded', 'true')
 
     this.classList.toggle('active');
+    document.body.classList.toggle('menu');
 
     target.classList.toggle('show');
 }
